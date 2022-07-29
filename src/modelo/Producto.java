f9efe079b04fe60044eff00b0268b0d7c44d800d
GetialJuan <getial.juan@correounivalle.edu.co>
2022-07-29 15:57:12 -0500

    Se definio Producto adecuadamente

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -9,8 +9,33 @@ package modelo;
  * @author Juan
  */
 public class Producto {
+    
+    private String nombre;
+    private int cantidad;
+    private int precio;
 
-    public Producto() {
+    public Producto(String nombre, int cantidad, int precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void reducirUnaUnidad(){
+        if(cantidad>0){
+            cantidad--;
+        }
     }
     
 }
